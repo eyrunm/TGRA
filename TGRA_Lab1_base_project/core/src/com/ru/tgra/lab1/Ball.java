e84feed5a35ee8c9bbc0294abf92d9edb17795c2
hrafnhildur <hrafnhildurs15@ru.is>
2017-09-06 13:28:38 +0000

    nothing

--- TGRA_Lab1_base_project/core/src/com/ru/tgra/lab1/Ball.java
+++ TGRA_Lab1_base_project/core/src/com/ru/tgra/lab1/Ball.java
@@ -1,12 +1,33 @@
 package com.ru.tgra.lab1;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
 public class Ball {
 	public Point2D coords;
-	//public float radius;
+	public float radius;
 	
-	public Ball(Point2D p) {
+	public Ball(Point2D p, float rad) {
 		this.coords = p;
-		//this.radius = rad;
+		this.radius = rad;
 		
+	}
+	
+	public void draw(int colorLoc) {
+		
+		Gdx.gl.glUniform4f(colorLoc, 0.7f, 0.2f, 0.4f, 1);  // pink
+		Gdx.gl.glDrawArrays(GL20.GL_TRIANGLE_FAN, 0, 3);
+		//Gdx.gl.glTranslatef(this.coords.getXFromPair(), this.coords.getYFromPair(), 0);
+		/*
+		Gdx.gl.glPushMatrix();
+
+		Gdx.gl.glTranslatef(this.get_middle().x, this.get_middle().y, 0);
+        
+		Gdx.gl.glScalef(4.0f, 4.0f, 4.0f);
+        Gdx.gl.glDrawArrays(GL20.GL_TRIANGLE_FAN, 4, 40);
+        Gdx.gl.glPopMatrix();
+        */
+        
+        
 	}
 }
